d26e57107bf5b504cbd40397cb346d3cafc53dfb
Nir Avraham <nir905@gmail.com>
2017-08-28 19:46:08 +0300

    Rollback to EditProfile,Regirtraion,BuisnessInformation

--- SecureDev/Controllers/EditProfileController.cs
+++ SecureDev/Controllers/EditProfileController.cs
@@ -6,9 +6,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.Security.Application;
 using Vladi2.App_Start;
-using Vladi2.Helpers;
 using Vladi2.Models;
 
 namespace Vladi2.Controllers
@@ -24,51 +22,132 @@ namespace Vladi2.Controllers
         [HttpPost]
         public ActionResult Index(User user, HttpPostedFileBase file)
         {
+            int permittedSizeInBytes = 4000000;//4mb
             string path = "";
-            ErrorValidation error = UserValidation.IsValidUser(user, "", false);
 
-            switch (error.ErrorCode)
+            Regex regex;
+            Match match;
+
+            if (user.FirstName != null)
             {
-                case ErrorValidation.Errors.UserNameTaken:
-                case ErrorValidation.Errors.NotValidUserName:
-                    ViewBag.userNameErr = error.Message;
+                regex = new Regex(@"^[a-zA-Z]{1,6}[-]{0,1}[a-zA-Z]{1,6}$");
+                match = regex.Match(user.FirstName);
+
+                if (!match.Success)
+                {
+                    ViewBag.firstNameErr = "First Name must be between 2 - 12 letters";
                     return View(user);
-                case ErrorValidation.Errors.NotValidPassword:
-                    ViewBag.passwordErr = error.Message;
-                    return View(user);
-                case ErrorValidation.Errors.NotValidFirstName:
-                    ViewBag.firstNameErr = error.Message;
-                    return View(user);
-                case ErrorValidation.Errors.NotValidLastName:
-                    ViewBag.lastNameErr = error.Message;
-                    return View(user);
-                case ErrorValidation.Errors.NotValidEmail:
-                    ViewBag.emailErr = error.Message;
-                    return View(user);
-                case ErrorValidation.Errors.NotValidPhone:
-                    ViewBag.phoneErr = error.Message;
-                    return View(user);
+                }
+                else
+                {
+                    ViewBag.firstNameErr = null;
+                }
             }
-
-            UploadFileValidation valid = UserValidation.UploadFile(file);
-            if (valid.ErrorCode == UploadFileValidation.Errors.Error)
+            else
             {
-                ViewBag.imageErr = valid.Message;
+                ViewBag.firstNameErr = "First Name must be between 2 - 12 letters";
                 return View(user);
             }
-            if (valid.Message != "")
-            {
-                file.SaveAs(Path.Combine(Server.MapPath(@"~\ProfileImages\"), valid.Message));
 
-                //delete prev picture
-                string oldFileName = Server.MapPath(((User)Session["myUser"]).PictureUrl);
-                if (!oldFileName.Contains("default-user.png"))
-                    if (System.IO.File.Exists(oldFileName))
-                    {
-                        System.IO.File.Delete(oldFileName);
-                    }
-                ((User)Session["myUser"]).PictureUrl = @"~/ProfileImages/" + valid.Message;
+            if (user.LastName != null)
+            {
+                regex = new Regex(@"^[a-zA-Z]{1,8}[-]{0,1}[a-zA-Z]{1,8}$");
+                match = regex.Match(user.LastName);
+
+                if (!match.Success)
+                {
+                    ViewBag.lastNameErr = "Last Name must be between 2 - 16 letters";
+                    return View(user);
+                }
+                else
+                {
+                    ViewBag.lastNameErr = null;
+                }
             }
+            else
+            {
+                ViewBag.lastNameErr = "Last Name must be between 2 - 16 letters";
+                return View(user);
+            }
+
+            if (user.Email != null)
+            {
+                regex = new Regex(@"^(([a-zA-Z0-9._%-]{2,30})@([a-zA-Z.-]{3,12})\.([a-zA-Z]{2,5}))$");
+                match = regex.Match(user.Email);
+
+                if (!match.Success)
+                {
+                    ViewBag.emailErr = "Not a valid email";
+                    return View(user);
+                }
+                else
+                {
+                    ViewBag.emailErr = null;
+                }
+            }
+            else
+            {
+                ViewBag.emailErr = "Not a valid email";
+                return View(user);
+            }
+
+            if (user.Phone != null)
+            {
+                regex = new Regex(@"^[0-9]{10,10}$");
+                match = regex.Match(user.Phone);
+
+                if (!match.Success)
+                {
+                    ViewBag.phoneErr = "Not a valid phone number";
+                    return View(user);
+                }
+                else
+                {
+                    ViewBag.phoneErr = null;
+                }
+            }
+            else
+            {
+                ViewBag.phoneErr = "Not a valid phone number";
+                return View(user);
+            }
+
+
+            if (file != null)
+            {
+                if (file.ContentType.ToLower() != "image/jpg" &&
+                        file.ContentType.ToLower() != "image/jpeg" &&
+                        file.ContentType.ToLower() != "image/pjpeg" &&
+                        file.ContentType.ToLower() != "image/gif" &&
+                        file.ContentType.ToLower() != "image/x-png" &&
+                        file.ContentType.ToLower() != "image/png" &&
+                        Path.GetExtension(file.FileName).ToLower() != ".jpg" &&
+                        Path.GetExtension(file.FileName).ToLower() != ".png" &&
+                        Path.GetExtension(file.FileName).ToLower() != ".gif" &&
+                        Path.GetExtension(file.FileName).ToLower() != ".jpeg")
+                {
+                    ViewBag.imageErr = "Only images are allowed";
+                    return View(user);
+                }
+
+                if (file.ContentLength > permittedSizeInBytes)
+                {
+                    ViewBag.imageErr = "image cannot be more than 4MB";
+                    return View(user);
+                }
+                else
+                    ViewBag.imageErr = null;
+
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName).ToLower();
+
+                path = Path.Combine(Server.MapPath(@"~\ProfileImages\"), fileName);
+                file.SaveAs(path);
+
+
+                ((User)Session["myUser"]).PictureUrl = @"~/ProfileImages/" + fileName;
+            }
+            else
+                ViewBag.imageErr = null;
 
             ((User)Session["myUser"]).FirstName = user.FirstName;
             ((User)Session["myUser"]).LastName = user.LastName;
