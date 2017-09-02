80ac161d249f52bb5ac6c613cc64769770a48b8f
lion_levy <lion_levy@localhost>
2017-09-02 14:12:20 +0300

    Finished AdminUsersController need to check if transaction and rollback is needed need to check if the page is really well protected

--- SecureDev/Controllers/AdminUsersController.cs
+++ SecureDev/Controllers/AdminUsersController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.Security.Application;
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vladi2.App_Start;
 using Vladi2.Models;
 
 namespace Vladi2.Controllers
 {
+    [AuthAttr(OnlyAdmin = true)]
     public class AdminUsersController : Controller
     {
         // GET: AdminUsers
@@ -41,8 +44,50 @@ namespace Vladi2.Controllers
             }
 
 
+            return View(usersList);
+        }
 
-                return View(usersList);
+
+        [HttpPost]
+        public ActionResult changeUsersPrivileges(List<User> usersList)
+        {
+            List<string> checkedUsers = new List<string>();
+
+            for (var i = 0; i < usersList.Count(); i++)
+            {
+                if (usersList[i].IsAdmin)
+                {
+                    usersList[i].UserName = Sanitizer.GetSafeHtmlFragment(usersList[i].UserName);
+                    //checkedUsers.Add(usersList[i].UserName);
+                    checkedUsers.Add("@name" + i);
+                }
+            }
+
+            string param = String.Join(",", checkedUsers.ToArray());
+
+            var connectionString = string.Format("DataSource={0}", Server.MapPath(@"~\Sqlite\db.sqlite"));
+            using (var m_dbConnection = new SQLiteConnection(connectionString))
+            {
+                m_dbConnection.Open();
+                SQLiteCommand updateTempPrivilegesCommand = new SQLiteCommand(@"UPDATE Users set isAdmin = 0", m_dbConnection);
+
+                updateTempPrivilegesCommand.ExecuteNonQuery();
+
+                SQLiteCommand updatePrivilegesCommand = new SQLiteCommand(@"UPDATE Users set isAdmin = 1
+                                                                            WHERE userName IN (" + param + ")", m_dbConnection);
+                for (int i = 0; i < usersList.Count(); i++)
+                {
+                    if (usersList[i].IsAdmin)
+                    {
+                        updatePrivilegesCommand.Parameters.Add(new SQLiteParameter("name" + i, usersList[i].UserName));
+                    }
+                }
+
+                updatePrivilegesCommand.ExecuteNonQuery();
+            }
+
+
+            return RedirectToAction("Index", "Admin");
         }
     }
 }
