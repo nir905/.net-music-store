75ac706b19393bc0fda5a3690519505b8150923d
Maria Naroditski <Maria Naroditski@localhost>
2017-09-17 14:42:12 +0300

    Finished EditCategories need to add athe page addCategory

--- SecureDev/Controllers/AdminCategoriesController.cs
+++ SecureDev/Controllers/AdminCategoriesController.cs
@@ -55,9 +55,40 @@ namespace Vladi2.Controllers
                 }
             }
 
-            //TODO: update DB
 
-            return RedirectToAction("Index", "Admin");
+
+            var connectionString = string.Format("DataSource={0}", Server.MapPath(@"~\Sqlite\db.sqlite"));
+            using (var m_dbConnection = new SQLiteConnection(connectionString))
+            {
+                m_dbConnection.Open();
+
+                string query = "";
+
+                for (int i = 1; i <= categories.Count(); ++i)
+                    query = query + "WHEN categoryID = @id" + i + " THEN @value" + i + " ";
+
+
+                using (SQLiteCommand updateCategory = new SQLiteCommand(@"UPDATE Category
+                                                                          SET categoryName = CASE " + query + " ELSE categoryName END", m_dbConnection))
+                {
+                    for (int i = 0; i < categories.Count(); ++i)
+                    {
+                        updateCategory.Parameters.Add(new SQLiteParameter("id" + (i + 1), categories[i].CategoryID));
+                        updateCategory.Parameters.Add(new SQLiteParameter("value" + (i + 1), categories[i].CategoryName));
+                    }
+
+                    updateCategory.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index", "AdminCategories");
+        }
+
+
+        //TODO: create 'add category page'
+        public ActionResult addCategory()
+        {
+            return View();
         }
     }
 }
